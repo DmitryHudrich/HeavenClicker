8a1248ffacdf4b0df1f82ad3696a6018ecce42e5
DmitryHudrich <dimaprudnikov9@gmail.com>
2023-06-06 00:37:53 +0300

    Дизайн в процессе.
    Добавил каркасы кнопок гитхаб телеграм вконтакте а также кнопки статистики и настроек
    Добавил текстбокс с название организации
    Добавил Главную кнопку
    Добавил отображение статы в виде текстбоксов под кнопкой
    Проделал небольшую работу над стилями.
    
    Я спать бля, дизайн планирую доделать завтра, после завтра займусь реализацией mvvm паттерна и тогда уже приступим к бекенду

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using HeavenClicker.ViewModels;
 
 namespace HeavenClicker
 {
@@ -13,6 +14,5 @@ namespace HeavenClicker
     /// </summary>
     public partial class App : Application
     {
-
     }
 }
